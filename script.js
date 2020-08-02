ab58ac08a4ef124bdaa0e2f9a8427d0b4047a01e
harlowevvh <67294180+vharnarine@users.noreply.github.com>
2020-08-01 17:53:06 -0700

    Updated comments on html, js & css files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// This is the checkbox version
+// This is the checkbox version of the password generator
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -14,48 +14,48 @@ slider.oninput = function() {
 
 // Write password to the #password input
 function writePassword() {
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numeric= "0123456789";
-  var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-  var lowercaseInput = document.getElementById("lowercase");
-  var uppercaseInput = document.getElementById("uppercase");
-  var numericInput = document.getElementById("numeric");
-  var specialCharactersInput = document.getElementById("specialCharacters");
-  var passwordlength = document.getElementById("myRange").value;
-  var newPassword = "";
-  var passwordCharacters = ""; 
-if(lowercaseInput.checked) {
-passwordCharacters += lowercase;
-}
-if(uppercaseInput.checked) {
-  passwordCharacters += uppercase;
-  }
-if(numericInput.checked) {
-  passwordCharacters += numeric;
-  } 
-if(specialCharactersInput.checked) {
-  passwordCharacters += specialCharacters;
-  } 
-if(!lowercaseInput.checked && 
-    !uppercaseInput.checked &&
-    !numericInput.checked &&
-    !specialCharactersInput.checked
-  )
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numeric = "0123456789";
+    var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    var lowercaseInput = document.getElementById("lowercase");
+    var uppercaseInput = document.getElementById("uppercase");
+    var numericInput = document.getElementById("numeric");
+    var specialCharactersInput = document.getElementById("specialCharacters");
+    var passwordlength = document.getElementById("myRange").value;
+    var newPassword = "";
+    var passwordCharacters = ""; 
+
+  //   Checks if user selected at least one criteria
+    if (lowercaseInput.checked) {
+        passwordCharacters += lowercase;
+    }
+    if (uppercaseInput.checked) {
+        passwordCharacters += uppercase;
+    }
+    if (numericInput.checked) {
+        passwordCharacters += numeric;
+    }
+    if (specialCharactersInput.checked) {
+        passwordCharacters += specialCharacters;
+    }
+    if (!lowercaseInput.checked &&
+        !uppercaseInput.checked &&
+        !numericInput.checked &&
+        !specialCharactersInput.checked
+    )
+//  Alert if user did not select at least one criteris
   {alert('Please select at least one criteria.')}
 
-  for (var i = 0; i < passwordlength; i++)
-  {newPassword += passwordCharacters.charAt(Math.floor(Math.random()* passwordCharacters.length))
-  }
-  document.getElementById("password").value=newPassword;
-  document.getElementById("myRange").oninput=function(){
-    if(document.getElementById("myRange").value>7){
-    } else {
-      document.getElementById('length').innerHTML="length: 8";
+// Loops through criteria selected and generates a random password
+    for (var i = 0; i < passwordlength; i++) {
+        newPassword += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length))
     }
-  }
+    document.getElementById("password").value = newPassword;
+    document.getElementById("myRange").oninput = function () {
+        if (document.getElementById("myRange").value > 7) {
+        } else {
+            document.getElementById('length').innerHTML = "length: 8";
+        }
+    }
 }
-
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
